60f3143043cfdd2da5e52c68b1ca8ec310bc0762
AlexManShrestha <alexm.bsc@rvu.edu.in>
2024-02-16 23:42:48 +0530

    test

--- set02/problem02.c
+++ set02/problem02.c
@@ -32,7 +32,10 @@ int check_scalene(int a, int b, int c)
 }
 void output(int a, int b, int c, int isscalene) 
 {
-    if (isscalene) {
+    
+    
+    
+     if (isscalene) {
         printf("The triangle with sides %d, %d, and %d is scalene.\n", a, b, c);
     } else {
         printf("The triangle with sides %d, %d, and %d is not scalene.\n", a, b, c);
